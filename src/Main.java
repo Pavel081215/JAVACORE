cbcdf8b97ce78a9af1507c5580f9913fdaa089e8
pavel081215 <pavelzag12@gmail.com>
2017-03-05 14:54:30 +0200

    3.2 комментарии

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        /**
+         * Это первый пример программы в главе 3 данного тома
+         * @version 1.0. 1997-03-22
+         */
     }
 }
